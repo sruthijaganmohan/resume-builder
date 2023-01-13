04bcca0166389acd1e0fe08c7d676bafc83c8f33
sruthijaganmohan <sruthi12jagan@gmail.com>
2023-01-13 18:28:35 +0530

    Rendered left column

---
+++ index.js
@@ -0,0 +1,34 @@
+// Upload and display profile picture
+const uploadImg = document.getElementById("upload-img");
+var image = " ";
+uploadImg.addEventListener("change", function() {
+    const reader = new FileReader();
+    reader.addEventListener("load", ()=> {
+        image = reader.result;
+        document.getElementById("display-img").style.backgroundImage = `url(${image})`
+        uploadImg.style.display = "none"
+    });
+    reader.readAsDataURL(this.files[0])
+})
+
+
+// Enter and dsplay skills
+let skillsList = []
+const skillsInput = document.getElementById("skills-input")
+const skillsUl = document.getElementById("skills-ul")
+const skillsBtn = document.getElementById("skills-btn")
+
+skillsBtn.addEventListener("click", function() {
+    skillsList.push(skillsInput.value)
+    skillsInput.value = ""
+    renderSkills()
+})
+
+function renderSkills() {
+    let listItems = ""
+    for (let i = 0; i < skillsList.length; i++) {
+        // Refactor the code below to use a template string
+        listItems += `<li>${skillsList[i]}</li>`
+    }
+    skillsUl.innerHTML = listItems  
+}
